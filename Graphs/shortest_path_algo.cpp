ba96663831e8ff27ffacc99873cec038b8a6e728
athakramgir <athakramgir39@gmail.com>
2026-08-31 17:04:09 +0530

    Bellman Ford Algorithm

--- Graphs/shortest_path_algo.cpp
+++ Graphs/shortest_path_algo.cpp
@@ -124,6 +124,35 @@ vector<int> dijkstra(int V, vector<vector<int>> &edges, int src){
     }
     return dist; 
 }
+
+/*-------------------------------------Bellman Ford Algorithm-------------------------------------------------*/
+// This algorithm is used to find shortest path from source to every vertex. Also, it helps identify a -ve cycle which in case of Dijkstra leads to an infinte loop=>TLE
+
+vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+    vector<int> dist(V, 1e8); 
+    dist[src] = 0; 
+    for(int i = 0; i < V-1; i++) {// V-1 times
+        for(auto it : edges) {
+            int u = it[0]; 
+            int v = it[1]; 
+            int wt = it[2]; 
+            if(dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                dist[v] = dist[u] + wt;  
+            }
+        }
+    }
+    // check for the negative cycle, Vth iteration 
+    for(auto it : edges) {
+        int u = it[0]; 
+        int v = it[1]; 
+        int wt = it[2]; 
+        if(dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+            return {-1}; 
+        }
+    }
+    return dist; 
+}
+
 int main(){
     
     return 0;
